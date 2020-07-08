e27d455ae440729313df88f819b8e658798e50ba
Alex Kvasha <alex.kvasha228@gmail.com>
2020-07-08 12:20:32 +0300

    Project RecipeShopping directives

---
+++ Projects/recipe-shopping/src/app/shared/dropdown.directive..ts
@@ -0,0 +1,13 @@
+import { Directive, HostListener, HostBinding, ElementRef } from '@angular/core';
+
+@Directive({
+    selector: "[appDropdown]"
+})
+export class DropdownDirective{
+    
+    @HostBinding("class.open")classAdded: boolean = false;
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.classAdded = this.elRef.nativeElement.contains(event.target) ? !this.classAdded : false;
+      }
+      constructor(private elRef: ElementRef) {}
+}
